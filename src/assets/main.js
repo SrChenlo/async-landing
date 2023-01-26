86d1e42fe09ea9f993fd2b0d05db1ffc27420680
SrChenlo <13chenlo@gmail.com>
2023-01-26 01:20:58 -0300

    Fix anchor

--- src/assets/main.js
+++ src/assets/main.js
@@ -21,20 +21,21 @@ async function fetchData(urlAPI) {
       .map(
         (video) => `
       <div class="group relative">
-        <div
+      <a href="https://www.youtube.com/watch?v=${video.id.videoId}">  
+      <div
           class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
           <img src="${video.snippet.thumbnails.medium.url}" alt="${video.snippet.description}" class="w-full">
         </div>
         <div class="mt-4 flex justify-between">
-          <h3 class="text-sm text-gray-700">
+          <h3 class="text-sm text-emerald-600 font-bold">
             <span aria-hidden="true" class="absolute inset-0"></span>
             ${video.snippet.title}
           </h3>
-        </div>
+        </div></a>
       </div>
     `
       )
-      .slice(0, 4)
+      .slice(1, 5)
       .join("")}
     `;
     content.innerHTML = view;
